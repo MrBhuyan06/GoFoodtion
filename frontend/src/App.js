47063bcb1c1fff123fd7850bc5cac24b18aa4e24
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-06-20 00:24:47 +0530

    Added the Init setructre of our app

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+      <header className="flex justify-center items-center w-screen h-screen">
+        <h1>App</h1>
+      </header>
+    </>
+  );
+};
+export default App;
