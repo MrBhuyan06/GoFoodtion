4b9059ea86f0587765d51d4ecba35a8d248a92d7
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-06-25 23:59:29 +0530

    added the brand section

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "";
 
 const Header = () => {
   return (
-    <div className="text-red-500">
-      <div className=" nav-lists  h-[200px]">
-        <ul>
-          <Link className="text-blue-400" to="/">
-            <li>Home</li>
-          </Link>
-          <Link to="/about">
-            <li className="text-blue-400">About</li>
-          </Link>
-          <Link to="/carts">
-            <li className="text-blue-400">Cart</li>
-          </Link>
-          <Link to="/menu">
-            <li className="text-blue-400">Menu</li>
-          </Link>
-        </ul>
-      </div>
+    <div className=" fixed z-50 w-screen  p-6 px-16    ">
+      {/* Desktop and tablet */}
+      <div className="hidden md:flex w-full h-full ">div</div>
+
+      {/* Mobile */}
+      <div className="flex  md:hidden w-full h-full "></div>
     </div>
   );
 };
