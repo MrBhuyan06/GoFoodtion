c70a3ca63e45cf88e3f99f24347df4f9a1d7620b
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-07-15 23:51:52 +0530

    fetch and aded all the food items

--- frontend/src/utils/firebaseFunction.js
+++ frontend/src/utils/firebaseFunction.js
@@ -1,10 +1,26 @@
 //save new items
 
-import { doc, setDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  orderBy,
+  query,
+  setDoc,
+} from "firebase/firestore";
 import { firestore } from "../firebase.config.js";
 
 export const saveItem = async (data) => {
   await setDoc(doc(firestore, "foodItems", `${Date.now()}`), data, {
     merge: true,
   });
+};
+
+//fetch all details
+export const getAllFoodItems = async () => {
+  const items = await getDocs(
+    query(collection(firestore, "foodItems"), orderBy("id", "desc"))
+  );
+  return items.docs.map((doc) => doc.data());
 };
