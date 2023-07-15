83d90c635b9539890b60eb386a1a666a75e3ca39
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-07-15 21:04:59 +0530

    Added the upload feature

--- frontend/src/components/MenuCreateContainer.js
+++ frontend/src/components/MenuCreateContainer.js
@@ -1,8 +1,23 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { MdFastfood } from "react-icons/md";
+import { saveItem } from "../utils/firebaseFunction.js";
+import {
+  MdFastfood,
+  MdCloudUpload,
+  MdDelete,
+  MdFoodBank,
+  MdCurrencyRupee,
+} from "react-icons/md";
 import { categories } from "../utils/constant.js";
-import loader from "./loader.js";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytes,
+  uploadBytesResumable,
+} from "firebase/storage";
+import Loaders from "./Loaders.js";
+import { storage } from "../firebase.config.js";
 const MenuCreateContainer = () => {
   const [tittle, setTittle] = useState("");
   const [calories, setCalories] = useState("");
@@ -10,8 +25,107 @@ const MenuCreateContainer = () => {
   const [category, setCategory] = useState(null);
   const [fields, setfields] = useState(false);
   const [msg, setMsg] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
   const [alterStatus, setalterStatus] = useState("danger");
   const [isLoading, setIsLoading] = useState(false);
+  const uploadImage = (e) => {
+    setIsLoading(true);
+    const imageFile = e.target.files[0];
+    console.log(imageFile);
+    const stoargeRef = ref(storage, `Images/${Date.now()}-${imageFile.name}`);
+    const uploadTask = uploadBytesResumable(stoargeRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const uploadProcess =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      },
+      (error) => {
+        console.log(error);
+        setfields(true);
+        setMsg("Error While uploading try agiain 😊");
+        setalterStatus("danger");
+        setTimeout(() => {
+          setfields(false);
+          setIsLoading(false);
+        }, 4000);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageAsset(downloadURL);
+          setIsLoading(false);
+          setfields(true);
+          setMsg("image uploaded Succesfully  😊");
+          setalterStatus("success");
+          setTimeout(() => {
+            setfields(false);
+          }, 4000);
+        });
+      }
+    );
+  };
+  const deleteImage = () => {
+    setIsLoading(true);
+    const deleteRef = ref(storage, imageAsset);
+    deleteObject(deleteRef).then(() => {
+      setImageAsset(null);
+      setIsLoading(false);
+      setMsg("image deleted Successfully");
+      setalterStatus("success");
+      setTimeout(() => {
+        setfields(false);
+      }, 4000);
+    });
+  };
+  const saveDetails = () => {
+    setIsLoading(true);
+    try {
+      if (!tittle || !calories || !imageAsset || !categories) {
+        setfields(true);
+        setMsg("Required Fields must be filled");
+        setalterStatus("danger");
+        setTimeout(() => {
+          setfields(false);
+          setIsLoading(false);
+        }, 4000);
+      } else {
+        const data = {
+          id: `${Date.now()}`,
+          tittle: tittle,
+          imageURL: imageAsset,
+          category: category,
+          calories: calories,
+          qty: 1,
+          price: price,
+        };
+        saveItem(data);
+        setIsLoading(false);
+        setMsg("Data uploaded Successfully");
+        setalterStatus("success");
+        clearData();
+        setTimeout(() => {
+          setfields(false);
+        }, 4000);
+      }
+    } catch (error) {
+      console.log(error);
+      setfields(true);
+      setMsg("Error While uploading try agiain 😊 man");
+      setalterStatus("danger");
+      setTimeout(() => {
+        setfields(false);
+        setIsLoading(false);
+      }, 4000);
+    }
+  };
+
+  const clearData = () => {
+    setTittle("");
+    setCalories("");
+    setCategory("Select Category");
+    setPrice("");
+    setImageAsset(null);
+  };
 
   return (
     <div className="w-full min-h-screen flex justify-center items-center  ">
@@ -37,7 +151,7 @@ const MenuCreateContainer = () => {
             required
             value={tittle}
             placeholder="Gie me a tittle"
-            className="w-full h-full text-lg bg-transparent font-semibold outline-none  border-none placeholder:text-gray-500"
+            className="w-full h-full text-lg bg-transparent outline-none  border-none placeholder:text-gray-500"
             onChange={(e) => {
               setTittle(e.target.value);
             }}
@@ -66,7 +180,85 @@ const MenuCreateContainer = () => {
           </select>
         </div>
         <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer">
-          {isLoading && <loader />}
+          {isLoading ? (
+            <Loaders />
+          ) : (
+            <>
+              {!imageAsset ? (
+                <>
+                  <label className="w-full  h-full flex flex-col items-center justify-center  cursor-pointer">
+                    <div className="w-full h-full flex flex-col items-center justify-center gap-4">
+                      <MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700" />
+                      <p className="text-gray-500 text-3xl hover:text-gray-700">
+                        click here to upload
+                      </p>
+                    </div>
+                    <input
+                      type="file"
+                      name="uploadimage"
+                      accept="image/*"
+                      onChange={uploadImage}
+                      className="w-0 h-0"
+                    />
+                  </label>
+                </>
+              ) : (
+                <>
+                  <div className=" relative h-full">
+                    <img
+                      src={imageAsset}
+                      alt="uploaded image"
+                      className="w-full h-full object-cover"
+                    />
+                    <button
+                      type="button"
+                      className="absolute bottom-3 right-3 p3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all  ease-in-out "
+                      onClick={deleteImage}
+                    >
+                      <MdDelete className="text-white" />
+                    </button>
+                  </div>
+                </>
+              )}
+            </>
+          )}
+        </div>
+        <div className="w-full flex flex-col md:flex-row items-center gap-3">
+          <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+            <MdFoodBank className="text-gray-700 text-2xl" />
+            <input
+              type="text"
+              required
+              placeholder="calories"
+              value={calories}
+              onChange={(e) => {
+                setCalories(e.target.value);
+              }}
+              className="w-fll h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
+            />
+          </div>
+          <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+            <MdCurrencyRupee className="text-gray-700 text-2xl" />
+            <input
+              type="text"
+              required
+              value={price}
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+              placeholder="price"
+              className="w-fll h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
+            />
+          </div>
+        </div>
+        <div className="flex items-center w-full">
+          <button
+            type="button"
+            className="ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg font-semibold text-headingColor"
+            onClick={saveDetails}
+          >
+            Save
+          </button>
         </div>
       </div>
     </div>
---
+++ frontend/src/utils/firebaseFunction.js
@@ -0,0 +1,10 @@
+//save new items
+
+import { doc, setDoc } from "firebase/firestore";
+import { firestore } from "../firebase.config.js";
+
+export const saveItem = async (data) => {
+  await setDoc(doc(firestore, "foodItems", `${Date.now()}`), data, {
+    merge: true,
+  });
+};
