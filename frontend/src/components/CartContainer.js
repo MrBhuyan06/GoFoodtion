e5c37806be5ff37014798e8dd20dea9d24cf77ce
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-07-19 22:03:36 +0530

    Added all the functionality

--- frontend/src/components/CartContainer.js
+++ frontend/src/components/CartContainer.js
@@ -11,6 +11,8 @@ import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
 
 import { motion } from "framer-motion";
 
+import EmtyCart from "../img/emptyCart.svg";
+
 const CartContainer = () => {
   const [{ user, cartShow, cartItems }, dispatch] = useStateValue();
   const [total, setTotal] = useState();
@@ -61,71 +63,89 @@ const CartContainer = () => {
         </motion.p>
       </div>
       {/* Bottom Section */}
-      <div className="w-full h-full bg-cartBg rounded-t-[20px] flex flex-col">
-        {/* Cart items Section */}
-        <div className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
-          {/* Cart Items */}
-          {cartItems &&
-            cartItems.map((item) => {
-              return (
-                <div className="w-full p-1 px-2 rounded-lg bg-cartItem flex items-center gap-2">
-                  <img
-                    src={item.imageURL}
-                    alt=""
-                    className="w-20 h-20  max-w-[60px] rounded-full object-contain "
-                  />
-                  {/* Name sectionnqd */}
-                  <div className="flex flex-col gap-2">
-                    <p className="text-base text-gray-100"> {item.tittle}</p>
-                    <p className="text-sm black text-gray-300 font-semibold">
-                      {item.price}
-                    </p>
-                  </div>
-                  {/* btn */}
-                  <div className="group flex items-center gap-2 ml-auto cursor-pointer">
-                    <motion.div whileTap={{ scale: 0.75 }}>
-                      {/* <MdPlue /> */}
+      {cartItems && cartItems.length > 0 ? (
+        <div className="w-full h-full bg-cartBg rounded-t-[20px] flex flex-col">
+          {/* Cart items Section */}
+          <div className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
+            {/* Cart Items */}
+            {cartItems &&
+              cartItems.map((item) => {
+                return (
+                  <div className="w-full p-1 px-2 rounded-lg bg-cartItem flex items-center gap-2">
+                    <img
+                      src={item.imageURL}
+                      alt=""
+                      className="w-20 h-20  max-w-[60px] rounded-full object-contain "
+                    />
+                    {/* Name sectionnqd */}
+                    <div className="flex flex-col gap-2">
+                      <p className="text-base text-gray-100"> {item.tittle}</p>
+                      <p className="text-sm black text-gray-300 font-semibold">
+                        {item.price}
+                      </p>
+                    </div>
+                    {/* btn */}
+                    <div className="group flex items-center gap-2 ml-auto cursor-pointer">
+                      <motion.div whileTap={{ scale: 0.75 }}>
+                        {/* <MdPlue /> */}
 
-                      <AiFillMinusCircle className="text-orange-400 text-xl" />
-                    </motion.div>
-                    <p className="w-5 h-5 rounded-sm bg-cartBg text-gray-100 flex items-center justify-center">
-                      1
-                    </p>
-                    <motion.div whileTap={{ scale: 0.75 }}>
-                      {/* <MdMinus /> */}
-                      <AiFillPlusCircle className=" text-orange-400 text-xl" />
-                    </motion.div>
+                        <AiFillMinusCircle className="text-orange-400 text-xl" />
+                      </motion.div>
+                      <p className="w-5 h-5 rounded-sm bg-cartBg text-gray-100 flex items-center justify-center">
+                        {item.qty}
+                      </p>
+                      <motion.div whileTap={{ scale: 0.75 }}>
+                        {/* <MdMinus /> */}
+                        <AiFillPlusCircle className=" text-orange-400 text-xl" />
+                      </motion.div>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
-        </div>
+                );
+              })}
+          </div>
 
-        {/* Cart Total Section */}
-        <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
-          <div className="w-full flex items-center justify-between">
-            <p className="text-gray-100 text-lg">Sub Total</p>
-            <p className="text-gray-100 text-lg">{total}</p>
+          {/* Cart Total Section */}
+          <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
+            <div className="w-full flex items-center justify-between">
+              <p className="text-gray-100 text-lg">Sub Total</p>
+              <p className="text-gray-100 text-lg">{total}</p>
+            </div>
+            <div className="w-full flex items-center justify-between">
+              <p className="text-gray-100 text-lg">Delivery</p>
+              <p className="text-gray-100 text-lg">{deliveryCharge}</p>
+            </div>
+            <div className="w-full border-b border-gray-600 my-2"></div>
+            <div className="w-full flex items-center justify-between">
+              <p className="text-gray-200 text-xl font font-semibold">Total</p>
+              <p className="text-gray-200 text-xl font font-semibold">
+                Rs {sumTotal}
+              </p>
+            </div>
+            {user ? (
+              <motion.button
+                whileTap={{ scale: 0.75 }}
+                className="bg-gradient-to-tr from-orange-400 to-orange-600 w-full p-2 rounded-full text-gray-500 text-lg my-2 hover:shadow-lg   duration-100 ease-out"
+              >
+                Check Out
+              </motion.button>
+            ) : (
+              <motion.button
+                whileTap={{ scale: 0.75 }}
+                className="bg-gradient-to-tr from-orange-400 to-orange-600 w-full p-2 rounded-full text-gray-500 text-lg my-2 hover:shadow-lg   duration-100 ease-out"
+              >
+                Login to Check
+              </motion.button>
+            )}
           </div>
-          <div className="w-full flex items-center justify-between">
-            <p className="text-gray-100 text-lg">Delivery</p>
-            <p className="text-gray-100 text-lg">{deliveryCharge}</p>
-          </div>
-          <div className="w-full border-b border-gray-600 my-2"></div>
-          <div className="w-full flex items-center justify-between">
-            <p className="text-gray-200 text-xl font font-semibold">Total</p>
-            <p className="text-gray-200 text-xl font font-semibold">
-              Rs {sumTotal}
-            </p>
-          </div>
-          <motion.button
-            whileTap={{ scale: 0.75 }}
-            className="bg-gradient-to-tr from-orange-400 to-orange-600 w-full p-2 rounded-full text-gray-500 text-lg my-2 hover:shadow-lg   duration-100 ease-out"
-          >
-            Check Out
-          </motion.button>
         </div>
-      </div>
+      ) : (
+        <div className="w-ful;l h-full flex flex-col items-center justify-center gap-6">
+          <img src={EmtyCart} alt="" />
+          <p className="text-xl text-textColor font-semibold">
+            Add some items to your cart
+          </p>
+        </div>
+      )}
     </motion.div>
   );
 };
--- frontend/src/components/MenuCardContainer.js
+++ frontend/src/components/MenuCardContainer.js
@@ -25,7 +25,7 @@ const MenuCardContainer = () => {
                   key={item.id}
                   className={`group ${
                     filters === item.urlParamName ? "scale-125 " : " bg-card"
-                  } bg-gradient-to-tr from-orange-400 to-orange-600  hover:bg-gradient-to-tr hover:from-orange-600 hover:to-orange-400 cursor-pointer w-24 min-w-[94px h-28  rounded-lg shadow-lg  drop-shadow-lg flex flex-col gap-2 items-center justify-center duration-150 transition-all ease-in`}
+                  } bg-gradient-to-tr from-orange-400 to-orange-600  hover:bg-gradient-to-tr hover:from-orange-600 hover:to-orange-400 cursor-pointer w-24 h-24 rounded-full min-w-[94px  shadow-lg  drop-shadow-lg flex flex-col gap-2 items-center justify-center duration-150 transition-all ease-in`}
                 >
                   <div className="w-10 h-10 rounded-full bg-red-600 group-hover:bg-card flex items-center justify-center">
                     <IoFastFood className="text-textColor text-2xl" />
